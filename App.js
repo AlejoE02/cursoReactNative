60ef323400fb4fa67527e6593e1c5be67029662d
Diego Alejandro Reyes Espitia <diego.reyes@tars.dev>
2019-10-30 12:32:41 -0500

    video 12 listas

--- App.js
+++ App.js
@@ -1,60 +1,21 @@
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-} from 'react-native';
+import React, {Component} from 'react';
+import {Text} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
+import SuggestionList from './src/videos/containers/suggestion-list';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Image
-          source={require('./assets/logo.png')}
-          style={{width:300, height: 80}}
-        />
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Home>
+        <Header></Header>
+        <Text>buscador</Text>
+        <Text>categorías</Text>
+        <Text>sugerencias</Text>
+        <SuggestionList />
+      </Home>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Platform.select({
-      ios: 'blue',
-      android: 'cyan',
-    }),
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ src/videos/containers/suggestion-list.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {FlatList, Text} from 'react-native';
+
+class SuggestionList extends Component {
+  render() {
+    const list = [
+      {
+        title: 'alejo',
+        key: '1',
+      },
+      {
+        title: 'espitia',
+        key: '2',
+      },
+    ];
+    return (
+      <FlatList data={list} renderItem={({item}) => <Text>{item.title}</Text>} />
+    );
+  }
+}
+
+export default SuggestionList;
