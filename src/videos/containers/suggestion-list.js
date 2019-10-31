4f94f6a7132ba7a17877733dccaef3ed53054d9e
Diego Alejandro Reyes Espitia <diego.reyes@tars.dev>
2019-10-31 16:54:51 -0500

    hasta el video 20

--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -2,24 +2,26 @@ import React, {Component} from 'react';
 import {FlatList, Text} from 'react-native';
 
 import Layout from '../components/suggestion-list-layout';
+import Empty from '../components/empty';
+import Separator from '../components/vertical-separator';
+import Suggestion from '../components/suggestion';
 
 class SuggestionList extends Component {
+  keyExtractor = item => item.id.toString();
+  renderEmpty = () => <Empty text="No hay recomendaciones para ti" />;
+  itemSeparator = () => <Separator />;
+  renderItem = ({item}) => {
+    return <Suggestion {...item} />;
+  };
   render() {
-    const list = [
-      {
-        title: 'titulo',
-        key: '1',
-      },
-      {
-        title: 'otro titulo',
-        key: '2',
-      },
-    ];
     return (
       <Layout title="Recomendado para ti">
         <FlatList
-          data={list}
-          renderItem={({item}) => <Text>{item.title}</Text>}
+          keyExtractor={this.keyExtractor}
+          data={this.props.list}
+          ListEmptyComponent={this.renderEmpty}
+          ItemSeparatorComponent={this.itemSeparator}
+          renderItem={this.renderItem}
         />
       </Layout>
     );
